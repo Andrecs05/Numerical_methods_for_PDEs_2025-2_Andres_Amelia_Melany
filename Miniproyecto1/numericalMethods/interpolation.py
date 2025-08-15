4a0641f49fe2242f552a5b89569b4a2563cddbf7
Amelia Hoyos <amelia327.hoyos@gmail.com>
2025-08-15 15:45:54 -0500

    :sparkles: feat: add interpolation data points and function to read them

--- Miniproyecto1/numericalMethods/interpolation.py
+++ Miniproyecto1/numericalMethods/interpolation.py
@@ -21,3 +21,20 @@ def lagrange_interpolation(points):
         poly += term
 
     return poly
+
+def 
+
+# Plot
+x = sp.symbols('x')
+
+point_sets = (len(chocolatera_points) - 1) // 2
+
+Volume = 0
+for i in range(point_sets - 1):
+    k = i + 1
+    expr = interpolation(chocolatera_points[2*k-1], chocolatera_points[2*k], chocolatera_points[2*k+1])
+    dV = sp.pi * expr**2
+    Volume += sp.integrate(dV, (x, chocolatera_points[2*k-1][0], chocolatera_points[2*k+1][0]))
+
+print(f"Volume of Chocolatera: {Volume.evalf()}")
+
--- Miniproyecto1/utilities.py
+++ Miniproyecto1/utilities.py
@@ -1,2 +1,21 @@
-def read_interpolation_points():
-    pass
+import matplotlib.pyplot as plt
+
+def read_interpolation_points(file_path):
+    points = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            x, y = map(float, line.strip().split(','))
+            points.append((x, y))
+    return points
+
+def plot_points(points):
+    x_vals, y_vals = zip(*points)
+    plt.figure(figsize=(8, 10))
+    plt.plot(x_vals, y_vals, marker='o', linestyle='-', color='blue')
+    plt.title('Swapped and Sorted Points Plot')
+    plt.xlabel('Original Y Coordinate (Now X)')
+    plt.ylabel('Original X Coordinate (Now Y)')
+    plt.grid(True)
+    plt.axis('equal')
+
+d
