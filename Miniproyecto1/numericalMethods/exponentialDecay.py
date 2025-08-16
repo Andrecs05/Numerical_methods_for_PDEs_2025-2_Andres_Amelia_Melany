bdb079e8e4ddbc7122d4ac4c7fc3d7b18bf32955
Andrecs05 <andresfcontreras@icloud.com>
2025-08-16 17:46:11 -0500

    :zap: Update function signatures to use T instead of steps

--- Miniproyecto1/numericalMethods/exponentialDecay.py
+++ Miniproyecto1/numericalMethods/exponentialDecay.py
@@ -1,33 +1,38 @@
 import numpy as np
 
-def forward_euler(u0, a, h, steps):
+def forward_euler(u0, a, h, T):
+    steps = int(T / h)
     u = np.zeros(steps+1)
     u[0] = u0
     for n in range(steps):
         u[n+1] = (1 - a*h) * u[n]
     return u
 
-def backward_euler(u0, a, h, steps):
+def backward_euler(u0, a, h, T):
+    steps = int(T / h)
     u = np.zeros(steps+1)
     u[0] = u0
     for n in range(steps):
         u[n+1] = u[n] / (1 + a*h)
     return u
 
-def crank_nicolson(u0, a, h, steps):
+def crank_nicolson(u0, a, h, T):
+    steps = int(T / h)
     u = np.zeros(steps+1)
     u[0] = u0
     for n in range(steps):
         u[n+1] = ((1 - 0.5*a*h) / (1 + 0.5*a*h)) * u[n]
     return u
 
-def theta_rule(u0, a, h, steps, theta):
+def theta_rule(u0, a, h, T, theta):
+    steps = int(T / h)
     u = np.zeros(steps+1)
     u[0] = u0
     for n in range(steps):
         u[n+1] = ((1 - (1-theta)*a*h) / (1 + theta*a*h)) * u[n]
     return u
 
-def exact_solution(u0, a, h, steps):
+def exact_solution(u0, a, h, T):
+    steps = int(T / h)
     t = np.linspace(0, steps*h, steps+1)
     return u0 * np.exp(-a*t)
